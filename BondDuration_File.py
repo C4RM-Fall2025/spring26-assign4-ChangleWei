10f9f30f143a60e43683b49a086fb3a991918fbc
ChangleWei <cw3755@columbia.edu>
2026-02-22 13:31:22 -0500

    Add bond duration calculation function
    Implement bond duration calculation based on yield, face value, coupon rate, and payment frequency.

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,21 @@
-
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    r = y / ppy
+    n = int(m * ppy)
+    c = face * couponRate / ppy
+
+    price = 0.0
+    weightSum = 0.0
+
+for t in range (1, n + 1):
+    cf = c
+    if t == n:
+        cf = c + face
+    pvcf = cf / ((1+r)**t)
+    
+    price += pvcf
+    weightsum += t * pvcf
+
+duration_periods = weightsum / price
+duration_years = duration_periods / ppy
+
+return durtion_years
