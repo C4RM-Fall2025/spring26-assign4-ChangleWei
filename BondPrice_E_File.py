df33ae761598445914412a3bc6944c345d368d4c
ChangleWei <cw3755@columbia.edu>
2026-02-22 16:23:24 -0500

    Implement getBondPrice_E function for bond pricing

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,14 @@
+def getBondPrice_E( face, couponRate, m, yc):
+    coupon = face * couponRate
+    bondPrice =  0.0
 
+    m = min(m, len(yc))
 
-def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    for t , y in enumerate(yc[:m], start = 1):
+        cf = coupon
+        if t == m:
+            cf = coupon + face
+        pv = 1 / ((1 + y) ** t )
+        bondPrice += cf * pv
+
+    return bondPrice
