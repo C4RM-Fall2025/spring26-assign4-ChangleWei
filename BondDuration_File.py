904d8b889781b0ca91fb0820e82a1697bed2eaa9
ChangleWei <cw3755@columbia.edu>
2026-02-22 13:34:23 -0500

    Fix variable naming and improve code readability

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,21 +1,20 @@
-def getBondDuration(y, face, couponRate, m, ppy = 1):
+def getBondDuration(y, face, couponRate, m, ppy=1):
     r = y / ppy
     n = int(m * ppy)
     c = face * couponRate / ppy
 
     price = 0.0
-    weightSum = 0.0
+    weighted_sum = 0.0
 
-for t in range (1, n + 1):
-    cf = c
-    if t == n:
-        cf = c + face
-    pvcf = cf / ((1+r)**t)
-    
-    price += pvcf
-    weightsum += t * pvcf
+    for t in range(1, n + 1):
+        cf = c
+        if t == n:
+            cf = c + face
 
-duration_periods = weightsum / price
-duration_years = duration_periods / ppy
+        pvcf = cf / ((1 + r) ** t)
+        price += pvcf
+        weighted_sum += t * pvcf
 
-return durtion_years
+    duration_periods = weighted_sum / price
+    duration_years = duration_periods / ppy
+    return duration_years
