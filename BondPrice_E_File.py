a12d8fb2e388f2fc5368fb29c75d9b47408165db
ChangleWei <cw3755@columbia.edu>
2026-02-22 16:27:41 -0500

    Refactor formatting and spacing in getBondPrice_E function

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,14 +1,15 @@
-def getBondPrice_E( face, couponRate, m, yc):
+def getBondPrice_E(face, couponRate, m, yc):
     coupon = face * couponRate
-    bondPrice =  0.0
+    bondPrice = 0.0
 
     m = min(m, len(yc))
 
-    for t , y in enumerate(yc[:m], start = 1):
+    for t, y in enumerate(yc[:m], start=1):
         cf = coupon
         if t == m:
             cf = coupon + face
-        pv = 1 / ((1 + y) ** t )
+
+        pv = 1 / ((1 + y) ** t)
         bondPrice += cf * pv
 
     return bondPrice
