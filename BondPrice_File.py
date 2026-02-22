957f3ec215d2e8687cff140ded8f40b777667d5f
ChangleWei <cw3755@columbia.edu>
2026-02-22 13:10:46 -0500

    Refactor getBondPrice to calculate price dynamically

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,11 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
+    r=y/ppy
+    n=int(m*ppy)
+    c=couponRate/ppy
+    x=0.0
+    for t in range(1, n+1):
+        x += c/((1+r)**t)
+    x += face/(1+r)**n) 
     return(x)
