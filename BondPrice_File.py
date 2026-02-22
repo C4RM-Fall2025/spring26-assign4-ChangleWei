84138e301deec1ef80051e133a3f7bf4a99a6a8c
ChangleWei <cw3755@columbia.edu>
2026-02-22 13:15:06 -0500

    Refactor getBondPrice function for clarity

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,11 +1,10 @@
-
-
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    r=y/ppy
-    n=int(m*ppy)
-    c=couponRate/ppy
-    x=0.0
-    for t in range(1, n+1):
-        x += c/((1+r)**t)
-    x += face/(1+r)**n) 
-    return(x)
+    r = y / ppy
+    n = int(m * ppy)
+    c = face * couponRate / ppy
+
+    price = 0.0
+    for t in range(1, n + 1):
+        price += c / ((1 + r) ** t)
+    price += face / ((1 + r) ** n)
+    return price
