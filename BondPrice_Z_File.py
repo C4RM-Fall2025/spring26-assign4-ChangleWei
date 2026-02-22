40f0c91896562342122dcdfe96d07e95a994391d
ChangleWei <cw3755@columbia.edu>
2026-02-22 16:45:54 -0500

    Implement getBondPrice_Z function for bond pricing

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,13 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    coupon = face * couponRate
+    bondPrice = 0.0
+
+    last_i = len(times) - 1
+
+    for i, (t, y) in enumerate(zip(times, yc)):
+        cf = coupon
+        if i == last_i:
+            cf = coupon + face
+        pv = 1 / ((1 + y) ** t)
+        bondPrice += cf * pv
+    return bondPrice
